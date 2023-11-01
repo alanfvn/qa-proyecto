cdb0bccce7300114cc06e4f07a9059b88a7fd16d
alanfvn <alandavidg16@gmail.com>
2023-11-01 17:58:22 -0600

    Agregue tests de navegación

---
+++ cypress/e2e/testsNavigation.cy.js
@@ -0,0 +1,50 @@
+describe('Tests de navegación', () => {
+  beforeEach(function () {})
+
+  it('Dirigirse al inicio', ()=>{
+    cy.clearCookies()
+    cy.visit('http://192.168.0.26')
+
+    const inputUser = cy.get('#id_username')
+    const inputPass = cy.get('#id_password')
+    inputUser.clear()
+    inputUser.type('admin')
+    inputPass.clear()
+    inputPass.type('admin')
+    cy.get('button[type="submit"]').click()
+  })
+
+  it('Dirigirse al modulo de reportes', ()=>{
+    cy.get('a[href="/reportes/"]').click()
+    cy.contains('h1', 'Reportes').should('exist');
+  })
+
+  it('Dirigirse al modulo de informes', ()=>{
+    cy.get('a[href="/informes/"]').click()
+    cy.contains('h1', 'Informes').should('exist');
+  })
+
+  it('Dirigirse al modulo de personal', ()=>{
+    cy.get('a[href="/personal/"]').click()
+    cy.contains('h1', 'Usuarios').should('exist');
+  })
+
+  it('Dirigirse al submodulo de servicios', ()=>{
+    cy.get('.nav-link.dropdown-toggle').click();
+    cy.get('a[href="/recursos/servicios/"]').click()
+    cy.contains('h1', 'servicios').should('exist');
+  })
+
+  it('Dirigirse al submodulo de vehiculos', ()=>{
+    cy.get('.nav-link.dropdown-toggle').click();
+    cy.get('a[href="/recursos/vehiculos/"]').click()
+    cy.contains('h1', 'Unidades').should('exist');
+  })
+
+  it('Dirigirse al submodulo de vehiculos', ()=>{
+    cy.get('.nav-link.dropdown-toggle').click();
+    cy.get('a[href="/recursos/hospitales/"]').click()
+    cy.contains('h1', 'Hospitales').should('exist');
+  })
+
+})
