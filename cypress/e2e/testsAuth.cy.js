6c58348056f2aa471418f158f5242047ee068771
alanfvn <alandavidg16@gmail.com>
2023-11-01 16:35:46 -0600

    Cree el primer set de tests

---
+++ cypress/e2e/testsAuth.cy.js
@@ -0,0 +1,28 @@
+describe('Tests de autenticación', () => {
+  beforeEach(function () {})
+
+  it('Verificar campos vacios', ()=>{
+    cy.visit('http://192.168.0.26')
+    cy.get('button[type="submit"]').click()
+    cy.url().should('contain','/auth/login/')
+  })
+
+  it('Verificar inició de sesión con mayúsculas', ()=>{
+    cy.get('#id_username').type('ADMIN')
+    cy.get('#id_password').type('admin')
+    cy.get('button[type="submit"]').click()
+    cy.get('div.alert').should('exist');
+  })
+
+  it('Verificar inició de sesión', () => {
+    const inputUser = cy.get('#id_username')
+    inputUser.clear()
+    inputUser.type('admin')
+    cy.get('#id_password').type('admin')
+    cy.get('button[type="submit"]').click()
+  })
+
+  it('Verificar cerrar sesión', ()=>{
+    cy.get('a[href="/auth/logout/"]').click();
+  })
+})
